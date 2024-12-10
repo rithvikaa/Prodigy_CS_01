05f0d48e57af6b996cf08017274dd8302b1e9598
Rithvika <114078260+rithvikaa@users.noreply.github.com>
2024-12-10 20:18:03 +0530

    Caesar cipher algo.py

--- Caesar cipher algo.py
+++ Caesar cipher algo.py
@@ -1,17 +1,21 @@
 alphabet={chr(i):i-97 for i in range(97,123)}
-def caesar_cipher_encrypt(text,shift):
-    encrypted_text=""
+def caesar_cipher(text,shift,mode):
+    result=""
     for char in text:
         if char.isalpha():
             char_lower=char.lower()
             char_num=alphabet[char_lower]
-            shifted_num=(char_num+shift)%26
-            encrypted_char=chr(shifted_num+97)if char.islower()else chr(shifted_num+65)
-            encrypted_text+=encrypted_char
+            if mode=="encrypt":
+                shifted_num=(char_num+shift)%26
+            elif mode=="decrypt":
+                shifted_num=(char_num-shift)%26
+            result+=chr(shifted_num+97)if char.islower()else chr(shifted_num+65)
         else:
-            encrypted_text+=char
-    return encrypted_text
+            result+=char
+    return result
+
 message=input("Enter your message: ")
 shift_value=int(input("Enter the shift value: "))
-encrypted_message=caesar_cipher_encrypt(message,shift_value)
-print(f"Encrypted message:{encrypted_message}")
+mode=input("Choose mode (encrypt/decrypt): ").lower()
+result=caesar_cipher(message,shift_value,mode)
+print(f"Result: {result}")
